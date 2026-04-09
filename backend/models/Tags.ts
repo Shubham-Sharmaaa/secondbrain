d651e06c9a1995cf6c64e576e96c32161490a842
Shubham Sharma <154622767+Shubham-Sharmaaa@users.noreply.github.com>
2026-04-10 03:42:36 +0530

    Delete backend directory

--- backend/models/Tags.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema;
-
-const TagSchema = new Schema({
-  title: String,
-});
-const TagModel = mongoose.model("Tags", TagSchema);
-
-export default TagModel;
